05ea2077f9a53bf47c882cdab317125123733d24
hungnt <acv12345>
2018-02-01 11:49:17 +0700

    Viết test cho action show trong controller

--- spec/controllers/products_controller_spec.rb
+++ spec/controllers/products_controller_spec.rb
@@ -1,20 +1,33 @@
 require 'rails_helper'
 
 RSpec.describe ProductsController, type: :controller do
-	let!(:category1) { create(:category) }
-	let!(:category2) { create(:category, title: 'Marshall') }
-	let!(:category3) { create(:category, title: 'Barney') }
+  let!(:category1) { create(:category) }
+  let!(:category2) { create(:category, title: 'Marshall') }
+  let!(:category3) { create(:category, title: 'Barney') }
+  let!(:product) { create(:product) }
 
-	describe '#index' do
-		it 'render index view' do
-			get :index
-			expect(response).to render_template :index
-		end
+  describe '#index' do
+    it 'render index view' do
+      get :index
+      expect(response).to render_template :index
+    end
 
-		it 'get a list of categories' do
-			categories = [category1, category2, category3]
-			get :index
-			expect(assigns(:categories)).to eq categories
-		end
-	end
+    it 'get a list of categories' do
+      categories = [category1, category2, category3]
+      get :index
+      expect(assigns(:categories)).to eq categories
+    end
+  end
+
+  describe '#show' do
+    it 'render show view' do
+      get :show, params: { id: product.id }
+      expect(response).to render_template :show
+    end
+
+    it 'show right selected product' do
+      get :show, params: { id: product.id }
+      expect(assigns(:product)).to eq product
+    end
+  end
 end
